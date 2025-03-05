b3e858875dbf0d18bdf00c6e2198c254e105d0c2
hemankpatwal <hemankpatwalcse@gmail.com>
2025-03-05 11:48:21 +0530

    feat: Add filtering to GET/expenses

--- index.js
+++ index.js
@@ -24,7 +24,19 @@ app.post('/expenses', async (req, res) => {
 
 app.get('/expenses', async (req, res) => {
     try {
-        const expenses = await Expense.find();
+
+        const { category, date } = req.query;
+
+        const filter = {};
+        if (category) filter.category = category;
+        if (date) {
+
+            const start = new Date(date);
+            const end = new Date(date);
+            end.setDate(end.getDate() + 1); 
+            filter.date = { $gte: start, $lt: end };
+        }
+        const expenses = await Expense.find(filter);
         res.status(200).send(expenses);
     } catch (error) {
         res.status(500).send({ error: error.message });
